9b46792be0487dcd2e61f6f9e2c2ed7fe50f974b
houyawei <1147439990@qq.com>
2020-12-13 20:03:19 +0800

    初始化项目

---
+++ DtkSerialport/DtkSerialport.pro
@@ -0,0 +1,11 @@
+QT += core gui dtkwidget
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = DtkSerialport
+TEMPLATE = app
+
+SOURCES += \
+        main.cpp
+
+RESOURCES +=         resources.qrc
---
+++ DtkSerialport/main.cpp
@@ -0,0 +1,33 @@
+#include <DApplication>
+#include <DMainWindow>
+#include <DWidgetUtil>
+#include <DApplicationSettings>
+
+DWIDGET_USE_NAMESPACE
+
+int main(int argc, char *argv[])
+{
+    DApplication::loadDXcbPlugin();
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    DApplication a(argc, argv);
+    a.setOrganizationName("deepin");
+    a.setApplicationName("dtk-application");
+    a.setApplicationVersion("1.0");
+    a.setProductIcon(QIcon(":/images/logo.svg"));
+    a.setProductName("Dtk Application");
+    a.setApplicationDescription("This is a dtk template application.");
+
+    a.loadTranslator();
+    a.setApplicationDisplayName(QCoreApplication::translate("Main", "DTK Application"));
+
+    // 保存程序的窗口主题设置
+    DApplicationSettings as;
+    Q_UNUSED(as)
+
+    DMainWindow w;
+    w.show();
+
+    Dtk::Widget::moveToCenter(&w);
+
+    return a.exec();
+}
